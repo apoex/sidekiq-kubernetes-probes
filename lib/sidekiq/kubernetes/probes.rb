95dec075ffb3e362f29a9025bc2bd89983a80379
Micke Lisinge <hi@micke.me>
2020-11-25 14:43:30 +0100

    Properly check for heartbeat method

--- lib/sidekiq/kubernetes/probes.rb
+++ lib/sidekiq/kubernetes/probes.rb
@@ -1,27 +1,24 @@
 require "sidekiq/kubernetes/probes/version"
 
+require "sidekiq/launcher"
+
 class SidekiqNotLoadedError < StandardError; end
 
-unless defined?(Sidekiq)
-  raise SidekiqNotLoadedError, "[sidekiq-kubernetes-probes] sidekiq is not loaded. " \
-    "Make sure that the sidekiq gem is loaded before `sidekiq-kubernetes-probes` in your Gemfile"
-end
-
-unless Sidekiq::Launcher.respond_to?(:heartbeat)
+unless Sidekiq::Launcher.private_instance_methods.include?(:heartbeat)
   raise SidekiqNotLoadedError, "[sidekiq-kubernetes-probes] I'm not compatible with this version of sidekiq. " \
     "Because Sidekiq::Launcher does not respond to the heartbeat method"
 end
 
 module Sidekiq
   class Launcher
+    private
+
     alias original_heartbeat heartbeat
 
     def heartbeat
       FileUtils.touch(File.join(root, "tmp", "sidekiq_health"))
       original_heartbeat
     end
-
-    private
 
     def root
       Dir.getwd
